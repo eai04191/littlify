258f1466e3df319719afa9be4a5730876a6d3025
Aoi Irie <eai@mizle.net>
2020-02-14 06:28:24 +0900

    SpotifySDK周りのイベントを分離

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -35,108 +35,101 @@ class App extends React.Component<Props, State> {
             state: state
         });
     };
+
+    injectSpotifyEvents = () => {
+        //@ts-ignore
+        window.onSpotifyWebPlaybackSDKReady = () => {
+            const token = this.props.qs.access_token;
+            //@ts-ignore
+            const player = new Spotify.Player({
+                name: "Littlify",
+                volume: 0.15,
+                //@ts-ignore
+                getOAuthToken: cb => {
+                    cb(token);
+                }
+            });
+            // Error handling
+            player.addListener(
+                "initialization_error",
+                //@ts-ignore
+                ({ message }) => {
+                    alert(message);
+                    location.href = "/";
+                    console.error(message);
+                }
+            );
+            player.addListener(
+                "authentication_error",
+                //@ts-ignore
+                ({ message }) => {
+                    alert(message);
+                    location.href = "/";
+                    console.error(message);
+                }
+            );
+            player.addListener(
+                "account_error",
+                //@ts-ignore
+                ({ message }) => {
+                    alert(message);
+                    location.href = "/";
+                    console.error(message);
+                }
+            );
+            player.addListener(
+                "playback_error",
+                //@ts-ignore
+                ({ message }) => {
+                    alert(message);
+                    location.href = "/";
+                    console.error(message);
+                }
+            );
+
+            player.addListener(
+                "player_state_changed",
+                //@ts-ignore
+                state => {
+                    this.handleStateChange(state);
+                }
+            );
+
+            // Ready
+            player.addListener(
+                "ready",
+                //@ts-ignore
+                ({ device_id }) => {
+                    console.log("Ready with Device ID", device_id);
+                }
+            );
+
+            // Not Ready
+            player.addListener(
+                "not_ready",
+                //@ts-ignore
+                ({ device_id }) => {
+                    console.log("Device ID has gone offline", device_id);
+                }
+            );
+
+            // Connect to the player!
+            player.connect();
+        };
+    };
+
     render() {
         return (
             <>
+                {this.props.qs.access_token && this.injectSpotifyEvents()}
                 {this.props.qs.access_token ? (
                     this.state.state ? (
                         <Player state={this.state.state} />
                     ) : (
-                        <>
-                            {
-                                //@ts-ignore
-                                (window.onSpotifyWebPlaybackSDKReady = () => {
-                                    const token = this.props.qs.access_token;
-                                    //@ts-ignore
-                                    const player = new Spotify.Player({
-                                        name: "Littlify",
-                                        volume: 0.15,
-                                        //@ts-ignore
-                                        getOAuthToken: cb => {
-                                            cb(token);
-                                        }
-                                    });
-                                    // Error handling
-                                    player.addListener(
-                                        "initialization_error",
-                                        //@ts-ignore
-                                        ({ message }) => {
-                                            alert(message);
-                                            location.href = "/";
-                                            console.error(message);
-                                        }
-                                    );
-                                    player.addListener(
-                                        "authentication_error",
-                                        //@ts-ignore
-                                        ({ message }) => {
-                                            alert(message);
-                                            location.href = "/";
-                                            console.error(message);
-                                        }
-                                    );
-                                    player.addListener(
-                                        "account_error",
-                                        //@ts-ignore
-                                        ({ message }) => {
-                                            alert(message);
-                                            location.href = "/";
-                                            console.error(message);
-                                        }
-                                    );
-                                    player.addListener(
-                                        "playback_error",
-                                        //@ts-ignore
-                                        ({ message }) => {
-                                            alert(message);
-                                            location.href = "/";
-                                            console.error(message);
-                                        }
-                                    );
-
-                                    player.addListener(
-                                        "player_state_changed",
-                                        //@ts-ignore
-                                        state => {
-                                            this.handleStateChange(state);
-                                        }
-                                    );
-
-                                    // Ready
-                                    player.addListener(
-                                        "ready",
-                                        //@ts-ignore
-                                        ({ device_id }) => {
-                                            console.log(
-                                                "Ready with Device ID",
-                                                device_id
-                                            );
-                                        }
-                                    );
-
-                                    // Not Ready
-                                    player.addListener(
-                                        "not_ready",
-                                        //@ts-ignore
-                                        ({ device_id }) => {
-                                            console.log(
-                                                "Device ID has gone offline",
-                                                device_id
-                                            );
-                                        }
-                                    );
-
-                                    // Connect to the player!
-                                    player.connect();
-                                })
-                            }
-                            <NoState />
-                        </>
+                        <NoState />
                     )
                 ) : (
-                    <>
-                        <LoginScreen />
-                    </>
+                    <LoginScreen />
                 )}
             </>
         );
