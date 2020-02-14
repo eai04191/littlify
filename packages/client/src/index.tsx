e2d9e84ed122a5383ec0ed8d0476825293503264
Aoi Irie <eai@mizle.net>
2020-02-14 09:08:48 +0900

    getOAuthToken()を実装

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -14,6 +14,8 @@ interface Props {
 
 interface State {
     state: SpotifyState.RootObject | null;
+    access_token: string | null;
+    refresh_token: string | null;
 }
 
 class App extends React.Component<Props, State> {
@@ -21,7 +23,9 @@ class App extends React.Component<Props, State> {
         super(props);
 
         this.state = {
-            state: null
+            state: null,
+            access_token: this.props.qs.access_token?.toString() || null,
+            refresh_token: this.props.qs.refresh_token?.toString() || null
         };
     }
 
@@ -36,17 +40,40 @@ class App extends React.Component<Props, State> {
         });
     };
 
-    injectSpotifyEvents = () => {
+    injectSpotifyEvents = ({ refresh_token }: { refresh_token: string }) => {
         //@ts-ignore
         window.onSpotifyWebPlaybackSDKReady = () => {
-            const token = this.props.qs.access_token;
             //@ts-ignore
             const player = new Spotify.Player({
                 name: "Littlify",
                 volume: 0.15,
-                //@ts-ignore
-                getOAuthToken: cb => {
-                    cb(token);
+                getOAuthToken: (cb: (arg0: string | null) => void) => {
+                    console.log("新しいアクセストークン取るよ");
+                    axios
+                        .get(`${process.env.SERVER_URI}/v1/refresh_token`, {
+                            params: { refresh_token: refresh_token }
+                        })
+                        .then(res => {
+                            if (!res.data.access_token) {
+                                throw Error(
+                                    "res.data.access_token is notfound"
+                                );
+                            }
+                            this.setState({
+                                access_token: res.data.access_token
+                            });
+                            console.log(
+                                "新しいアクセストークン: ",
+                                this.state.access_token
+                            );
+                            cb(this.state.access_token);
+                        })
+                        .catch(error =>
+                            console.log(
+                                "新しいアクセストークンの取得中にエラー",
+                                error
+                            )
+                        );
                 }
             });
             // Error handling
@@ -121,8 +148,12 @@ class App extends React.Component<Props, State> {
     render() {
         return (
             <>
-                {this.props.qs.access_token && this.injectSpotifyEvents()}
-                {this.props.qs.access_token ? (
+                {this.state.refresh_token &&
+                    this.injectSpotifyEvents({
+                        refresh_token: this.state.refresh_token
+                    })}
+
+                {this.state.access_token ? (
                     this.state.state ? (
                         <Player state={this.state.state} />
                     ) : (
