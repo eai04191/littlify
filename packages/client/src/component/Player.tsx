75acb251231826f32342838cf3d4061fd4d6758c
mohemohe <mohemohe@ghippos.net>
2020-03-21 02:23:56 +0900

    upstreamのController分離に対応

--- packages/client/src/component/Player.tsx
+++ packages/client/src/component/Player.tsx
@@ -12,25 +12,8 @@ interface Props {
     player: Spotify.SpotifyPlayer;
 }
 
-interface State {
-    calculatedPosition: number;
-    updatedPosition: number;
-    onSeek: boolean;
-}
-
-export default class Player extends React.Component<Props, State> {
-    private intervalHandler?: any;
+export default class Player extends React.Component<Props, {}> {
     shortcut = new Shortcut(this.props.player);
-
-    constructor(props: Props) {
-        super(props);
-
-        this.state = {
-            calculatedPosition: 0,
-            updatedPosition: 0,
-            onSeek: false,
-        };
-    }
 
     componentDidMount(): void {
         this.shortcut.enable();
@@ -38,30 +21,6 @@ export default class Player extends React.Component<Props, State> {
 
     componentWillUnmount(): void {
         this.shortcut.disable();
-    }
-
-    public componentDidUpdate(
-        prevProps: Readonly<Props>,
-        prevState: Readonly<{}>,
-        snapshot?: any
-    ) {
-        if (this.props.state.paused || this.state.onSeek) {
-            this.stopSeekBar();
-        } else if (!this.props.state.paused && !this.intervalHandler) {
-            this.startSeekBar();
-        } else {
-            // たぶん30秒ごとに飛んでくるstateで補正
-            // REF: https://github.com/spotify/web-playback-sdk/issues/86
-            let { calculatedPosition, updatedPosition } = this.state;
-            if (this.props.state.position !== updatedPosition) {
-                updatedPosition = this.props.state.position;
-                calculatedPosition = this.props.state.position;
-                this.setState({
-                    calculatedPosition,
-                    updatedPosition,
-                });
-            }
-        }
     }
 
     render() {
@@ -156,32 +115,6 @@ export default class Player extends React.Component<Props, State> {
                                 </p>
                             </div>
                         </div>
-                        <input
-                            id={"player-seekbar"}
-                            type="range"
-                            min="0"
-                            max={state.duration}
-                            value={this.state.calculatedPosition}
-                            onMouseDown={e => {
-                                this.setState({
-                                    onSeek: true,
-                                });
-                            }}
-                            onChange={e => {
-                                this.setState({
-                                    calculatedPosition: parseInt(
-                                        e.currentTarget.value,
-                                        10
-                                    ),
-                                });
-                            }}
-                            onMouseUp={e => {
-                                this.props.player?.seek(e.currentTarget.value);
-                                this.setState({
-                                    onSeek: false,
-                                });
-                            }}
-                        />
                         <Controller
                             state={this.props.state}
                             player={this.props.player}
